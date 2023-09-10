224a4b3eba2a05bbbfef8d675286757a2e058a03
Pamelazee <maarnea@gmail.com>
2023-09-10 22:50:48 +0200

    project done

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 
 
 const slackDataInfo = () => {
-    let slackDisplayImage = document.querySelector('[data-testid="slackDisplayImage"]')
-    let slackUserName = document.querySelector('[data-testid="slackUserName"]')
-    let currentDayOfTheWeek = document.querySelector('[data-testid="currentDayOfTheWeek"]')
-    let currentUTCTime = document.querySelector('[data-testid="currentUTCTime"]')
-    let myTrack = document.querySelector('[data-testid="myTrack"]')
-    let githubURL = document.querySelector('[data-testid="githubURL"]')
+    let slackDisplayImage = document.querySelector('[data-testid="slackDisplayImage"]');
+    let slackUserName = document.querySelector('[data-testid="slackUserName"]');
+    let currentDayOfTheWeek = document.querySelector('[data-testid="currentDayOfTheWeek"]');
+    let currentUTCTime = document.querySelector('[data-testid="currentUTCTime"]');
+    let myTrack = document.querySelector('[data-testid="myTrack"]');
+    let githubURL = document.querySelector('[data-testid="githubURL"]');
     // getting the current day
     const currentDayName = () => {
         const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
@@ -22,12 +22,16 @@ const slackDataInfo = () => {
         const hours = now.getHours().toString().padStart(2, '0');
         return `${hours}:${minutes}:${seconds}`;
       }
+    
+
       slackDisplayImage.innerHTML = `<img src= 'https://ca.slack-edge.com/T05FFAA91JP-U05RH96SJ8K-0dbb0c7ca85f-512'/>`
     slackUserName.innerText = "zika"
-    currentDayOfTheWeek.innerText = currentDayName()
-    currentUTCTime.innerText = currentTime()
-    myTrack.innerText = "Frontend"
-    githubURL.innerHTML = `<a href="https://github.com/jahseed89/slack-info" target="_blank">Link to Repository</a>`
+    currentDayOfTheWeek.innerText = currentDayName();
+    //currentUTCTime.innerText = currentTime()
+    currentUTCTime.innerText = currentTime();
+    
+   myTrack.innerText = "Frontend"
+    githubURL.innerHTML = `<a href="https://github.com/Pamelazee/HNG-TASK-1" target="_blank">Link to Repository</a>`
 
 }
 
